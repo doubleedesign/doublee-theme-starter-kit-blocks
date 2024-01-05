c4d39e9ae60d832b2802f432fd3fc31b4b82d46b
doubleedesign <leesa@doubleedesign.com.au>
2024-01-05 14:54:08 +1100

    Fix: Partial loading path

--- app/public/wp-content/themes/starterkit-blocks/partials/content.php
+++ app/public/wp-content/themes/starterkit-blocks/partials/content.php
@@ -2,6 +2,7 @@
 global $post;
 $blocks = parse_blocks($post->post_content);
 foreach($blocks as $block) {
-	$name = $block['blockName'];
-	get_template_part("partials/blocks/$name", '', array('block' => $block));
+	$full_name = $block['blockName'];
+	$short_name = str_replace('custom/', '', $full_name);
+	get_template_part("blocks/$full_name/$short_name", '', array('block' => $block));
 }
